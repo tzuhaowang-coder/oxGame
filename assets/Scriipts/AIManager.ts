c221b2675fc64d784a451291715eb3ed810a4ed7
tzuhao.wang <tzuhao.wang@MacBook-Air-2.local>
2026-09-02 11:37:56 +0800

    init

---
+++ assets/Scriipts/AIManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('AIManager')
+export class AIManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/Scriipts/ViewManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ViewManager')
+export class ViewManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
